c8dab0553c29526b232654d23d4bb33c07427d49
nataliia_petrukhina <natali.insajt@gmail.com>
2025-03-17 22:15:26 +0100

    Ich habe QuizPage,Quize.jsx, quizeData.js erstellen... Und auch habe ich mit Design slielen, GoogleFonts addieren nur für h1

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+      extend: {
+        fontFamily: {
+          barriecito: ['Barriecito', 'cursive'],
+        },
+      },
+    },
+    plugins: [],
+  };
