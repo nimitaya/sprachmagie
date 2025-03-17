e6d342446d85810a6e5a8262817355ad68b0e0c0
nimitaya <sonja.schweren@web.de>
2025-03-17 13:53:26 +0100

    test kurz

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@
 
 const Header = () => {
   return (
-    <header className="p-3 flex justify-between items-center sm:p-5">
+    <header className="p-3 flex justify-between items-center bg-amber-500 sm:p-5">
       <img src="./vite.svg" alt="Logo Placeholder" />
       <h1 className="text-xl">Sprachmagie</h1>
       <nav className="flex flex-col sm:flex-row">
         <ul className="flex flex-col sm:flex-row">
             <li className="m-2 p-2 border-2 rounded-lg sm:m-4 sm:p-4">Lernseite</li>
             <li className="m-2 p-2 border-2 rounded-lg sm:m-4 sm:p-4">Profil</li>
-            {/* TODO anstatt Profil Profilbild einfügen ?*/}
+            {/* TODO nicht nur Schrift, sondern auch Bilder einfügen*/}
         </ul>
         {/* <NavLink to="/lernseite">Lernseite</NavLink>
         <NavLink to="/punkte">Punktestand</NavLink> */}
