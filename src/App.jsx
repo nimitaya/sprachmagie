e2eaefc8a67f48a2544e8ecf85d08825cbcd7bf3
Sonja / Nimitaya <sonja.schweren@web.de>
2025-03-17 15:25:47 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 
-
-
+import Footer from "./components/Footer";
+import Lernseite from "./pages/Lernseite";
+import HomePage from "./pages/HomePage";
+function App() {
+  return 
+  <>
+  <HomePage />
+  <Lernseite />
+  <Footer />
+    </>
+ }
+  
+ export default App;
    
 
 
