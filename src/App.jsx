d36bbb8bfc4ad7872a6278d27541de8ffb0e4652
Sonja / Nimitaya <sonja.schweren@web.de>
2025-03-17 15:28:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Footer from "./components/Footer";
 import Lernseite from "./pages/Lernseite";
 import HomePage from "./pages/HomePage";
 function App() {
-  return 
+  return (
   <>
   <HomePage />
   <Lernseite />
   <Footer />
-    </>
+    </>)
  }
   
  export default App;
