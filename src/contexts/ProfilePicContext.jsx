44afc0b1d0460035483a7d478e0a89ec01d0dbd1
nimitaya <sonja.schweren@web.de>
2025-03-18 07:49:51 +0100

    add different pic for every click

--- src/contexts/ProfilePicContext.jsx
+++ src/contexts/ProfilePicContext.jsx
@@ -1,22 +1,30 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
 import { profilbilder } from "../data/profilePics";
 
-export const ProfilePicContext = createContext()
+export const ProfilePicContext = createContext();
 
-const ProfilePicContextProvider = ({children}) => {
+const ProfilePicContextProvider = ({ children }) => {
   const [profilePic, setProfilePic] = useState(profilbilder[0]);
+  const [prevIndex, setPrevIndex] = useState(0);
 
   const changeProfilePic = () => {
-    const randomIndex = Math.floor(Math.random() * profilbilder.length);
+    let randomIndex = Math.floor(Math.random() * profilbilder.length);
+    // to change Pic everytime you click
+    while (randomIndex === prevIndex) {
+      randomIndex = Math.floor(Math.random() * profilbilder.length);
+    }
     setProfilePic(profilbilder[randomIndex]);
+    setPrevIndex(randomIndex);
   };
 
-    const valueObject = {profilePic, changeProfilePic}
+  const valueObject = { profilePic, changeProfilePic };
 
   return (
-    <ProfilePicContext.Provider value={valueObject}>{children}</ProfilePicContext.Provider>
-  )
-}
-export default ProfilePicContextProvider
+    <ProfilePicContext.Provider value={valueObject}>
+      {children}
+    </ProfilePicContext.Provider>
+  );
+};
+export default ProfilePicContextProvider;
 
 // IMPORTANT add Context to main.jsx
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ProfilePicContextProvider from './contexts/ProfilePicContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ProfilePicContextProvider>
-
     <App />
-    </ProfilePicContextProvider>
   </StrictMode>,
 )
