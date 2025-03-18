fb564095330b6efadad8610420c51e1c66569e09
nimitaya <sonja.schweren@web.de>
2025-03-18 08:03:26 +0100

    add context for points and progress

--- src/components/ProgressPoints.jsx
+++ src/components/ProgressPoints.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import { ProgressContext } from "../contexts/ProgressContext";
+
 const ProgressPoints = () => {
+const {earnedPoints} = useContext(ProgressContext)
+
   return (
     <div className="p-4 flex justify-between">
       {/* Badge above Progress Bar */}
@@ -10,8 +15,8 @@ const ProgressPoints = () => {
       {/* Number */}
       <div className="w-28 aspect-square bg-indigo-100 rounded-full flex justify-center items-center self-center">
         <span className="text-xl font-semibold inline-block text-teal-600">
-          {/* TODO number needs to be dynamic */}
-          70
+          {/* shows earned points from context */}
+          {earnedPoints}
         </span>
       </div>
     </div>
