50cc05fcaefa864dc78db13ce0afaa91ade3271d
nataliia_petrukhina <natali.insajt@gmail.com>
2025-03-19 15:51:14 +0100

    Änderung mit css und Tailwind

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     <div
       className="flex flex-col items-center min-h-screen bg-amber-100 p-4 md:p-8 bg-cover bg-center relative"
       style={{
-        backgroundImage: `url('https://64.media.tumblr.com/35b0676ce21fc9a9471ab45b1e241444/tumblr_ol6o615mfP1qa9gmgo5_1280.jpg')`,
+        backgroundImage: `url('https://64.media.tumblr.com/1ada14b60cf5b70a887b045d6e83fbd4/tumblr_ol6o615mfP1qa9gmgo2_1280.jpg')`,
       }}
     >
       {/* backgtound */}
@@ -22,28 +22,28 @@ const HomePage = () => {
           />
         </div>
 
-        <h1 className="font-barriecito text-4xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center text-indigo-400">
+        <h1 className="font-barriecito text-4xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-center text-pink-200">
           Sprachmagie
         </h1>
-        <h2 className="text-center text-xl sm:text-2xl md:text-2xl lg:text-2xl xl:text-2xl text-emerald-600 mt-7">
+    {/*     <h2 className="text-center text-xl sm:text-3xl md:text-2xl lg:text-3xl xl:text-2xl text-emerald-200 mt-7">
           Jede Sprache ist ein Abenteuer
-        </h2>
+        </h2> */}
 
-        <div className="w-full max-w-xs md:max-w-md lg:max-w-lg bg-indigo-400 p-6 rounded-lg shadow-xl mt-11">
+        <div className="w-full max-w-xs md:max-w-md lg:max-w-lg bg-pink-200 p-6 rounded-lg shadow-xl mt-11">
           <div className="mt-4">
             <h2 className="text-lg font-semibold text-center mb-5">Login:</h2>
             <form className="mt-2 flex flex-col">
               <label className="mb-1 font-medium"></label>
               <input
                 type="text"
-                className="p-2 border rounded mb-2 focus:outline-none focus:ring-2 border-amber-300 focus:ring-amber-300 text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 via bg-amber-400 to-yellow-200 text-xl"
+                className="p-2 border-3 rounded mb-2 focus:outline-none focus:ring-2 border-amber-300 focus:ring-amber-300 text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via bg-purple-300 to-purple-400 text-2xl "
                 placeholder="Gib deinen Namen an"
               />
 
               <label className="mb-5 font-medium"></label>
               <input
                 type="password"
-                className="p-2 border rounded mb-4 focus:outline-none focus:ring-2 border-amber-300 focus:ring-amber-300 text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 via bg-amber-400 to-yellow-200 text-xl"
+                className="p-2  border-3 rounded mb-4 focus:outline-none focus:ring-2 border-amber-300 focus:ring-amber-300 text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via bg-purple-300 to-purple-400 text-2xl  "
                 placeholder="Gib dein Passwort ein"
               />
 
@@ -56,7 +56,7 @@ const HomePage = () => {
             </form>
 
             <Link to="/register">
-              <button className="mt-4 text-lg bg-amber-400 text-white py-2 rounded hover:bg-amber-300 transition w-full">
+              <button className="mt-4 text-xl bg-amber-400 text-white py-3 rounded hover:bg-amber-300 transition w-full">
                 Neu registrieren
               </button>
             </Link>
