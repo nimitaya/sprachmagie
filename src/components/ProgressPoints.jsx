b78a24d215cfe7351a6af00d5efd8dcc36bf2402
nimitaya <sonja.schweren@web.de>
2025-03-19 14:25:10 +0100

    styled profile a little bit

--- src/components/ProgressPoints.jsx
+++ src/components/ProgressPoints.jsx
@@ -5,15 +5,15 @@ const ProgressPoints = () => {
 const {earnedPoints} = useContext(ProgressContext)
 
   return (
-    <div className="p-4 flex justify-between">
+    <div className="p-4 w-full flex justify-between">
       {/* Badge above Progress Bar */}
       <div className="flex m-2 items-center">
-        <span className="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-indigo-400 bg-indigo-100">
+        <span className="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-indigo-400 bg-amber-100">
           Punkte
         </span>
       </div>
       {/* Number */}
-      <div className="w-28 aspect-square bg-indigo-100 rounded-full flex justify-center items-center self-center">
+      <div className="w-28 aspect-square bg-amber-100 rounded-full flex justify-center items-center self-center">
         <span className="text-3xl font-semibold inline-block text-indigo-400">
           {/* shows earned points from context */}
           {earnedPoints}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -11,7 +11,7 @@ const Profile = () => {
   return (
     <main className="max-w-sm mx-auto flex flex-col items-center flex-1">
       <h2 className="text-4xl m-5 text-indigo-400">
-        {appState.currentUser ? appState.currentUser.username : "Anonym"}
+        {appState.currentUser ? appState.currentUser.username.toUpperCase() : "ANONYM"}
       </h2>
       <div className=" w-40 aspect-square bg-indigo-100 rounded-full border-10 border-purple-500 flex justify-center items-center self-center shadow-lg cursor-pointer transition-transform transform hover:scale-105">
         {/* shows random profile picture when you click on it */}
