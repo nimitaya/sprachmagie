cb632372df9bf74a68a3c7231dad409cb0aaa2de
nimitaya <sonja.schweren@web.de>
2025-03-19 16:08:32 +0100

    add language select to pages

--- src/components/LanguageSelect.jsx
+++ src/components/LanguageSelect.jsx
@@ -1,9 +1,10 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { LanguageContext } from "../contexts/LanguageContext";
 
 const LanguageSelect = () => {
   const { currentLanguage, setCurrentLanguage } = useContext(LanguageContext);
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null); 
 
   const handleLanguageSelect = (language) => {
     switch (language) {
@@ -35,17 +36,32 @@ const LanguageSelect = () => {
       default:
         break;
     }
+    setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false); 
+        // Close the dropdown if clicked outside
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="w-full p-4 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
-      <p className="uppercase text-xl font-semibold text-indigo-400 bg-amber-100 py-1 px-2 rounded-full">
+    <div className="p-4 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
+      <p className="uppercase text-2xl text-center font-semibold text-indigo-400 bg-amber-100 py-1 px-2 rounded-full">
         Sprache
       </p>
-      <div className="dropdown inline-block relative">
+      <div ref={dropdownRef} className="dropdown inline-block relative">
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="bg-purple-700 text-gray-100 font-semibold min-w-36 py-2 px-4 rounded inline-flex items-center cursor-pointer hover:bg-purple-600 transition"
+          className="bg-purple-700 text-gray-100 text-2xl font-semibold min-w-44 py-2 px-4 rounded inline-flex justify-between items-center cursor-pointer hover:bg-purple-600 transition"
         >
           <span className="mr-1">{currentLanguage.full}</span>
           <svg
@@ -57,9 +73,9 @@ const LanguageSelect = () => {
           </svg>
         </button>
         <ul
-          className={`dropdown-menu absolute min-w-36 ${
+          className={`dropdown-menu absolute min-w-44 ${
             isOpen ? "" : "hidden"
-          } text-indigo-400 font-semibold pt-1`}
+          } text-indigo-400 text-2xl font-semibold pt-1 z-50`}
         >
           <li onClick={() => handleLanguageSelect("en")} className="">
             <a
