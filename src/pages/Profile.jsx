c5fdc9f207aea1b0ae694d2106d855d91ffc3daa
nimitaya <sonja.schweren@web.de>
2025-03-19 15:23:33 +0100

    add language select

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import ProgressPoints from "../components/ProgressPoints";
 import ProgressQuiz from "../components/ProgressQuiz";
+import LanguageSelect from "../components/LanguageSelect";
 import { ProfilePicContext } from "../contexts/ProfilePicContext";
 import { UserContext } from "../contexts/UserContext";
 
@@ -11,9 +12,11 @@ const Profile = () => {
   return (
     <main className="max-w-sm mx-auto flex flex-col items-center flex-1">
       <h2 className="text-4xl m-5 text-indigo-400">
-        {appState.currentUser ? appState.currentUser.username.toUpperCase() : "ANONYM"}
+        {appState.currentUser
+          ? appState.currentUser.username.toUpperCase()
+          : "ANONYM"}
       </h2>
-      <div className=" w-40 aspect-square bg-indigo-100 rounded-full border-10 border-purple-500 flex justify-center items-center self-center shadow-lg cursor-pointer transition-transform transform hover:scale-105">
+      <div className=" w-40 aspect-square bg-indigo-100 rounded-full border-10 border-purple-700 flex justify-center items-center self-center shadow-lg cursor-pointer transition-transform transform hover:scale-105">
         {/* shows random profile picture when you click on it */}
         <img
           className="rounded-full h-full w-auto object-cover"
@@ -24,6 +27,7 @@ const Profile = () => {
       </div>
       <ProgressPoints />
       <ProgressQuiz />
+      <LanguageSelect />
     </main>
   );
 };
