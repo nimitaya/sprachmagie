dbfb9f6809ca0a6c30b80dfac95a4ef74c6dad3f
nimitaya <sonja.schweren@web.de>
2025-03-20 16:28:23 +0100

    add styling to picQuiz categories

--- src/pages/PicQuizCategories.jsx
+++ src/pages/PicQuizCategories.jsx
@@ -10,9 +10,16 @@ const PicQuizCategories = () => {
   };
 
   return (
-    <main>
-      <h1 className="text-center p-4 my-4 text-4xl sm:text-6xl">Kategorien</h1>
-
+    <main style={{
+      backgroundImage:
+        "url('https://pbs.twimg.com/media/FOtUhw6XEAYt1sP.jpg:large')",
+      backgroundBlendMode: "overlay",
+      backgroundPosition: "center",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      fontFamily: '"Indie Flower", cursive',
+    }}>
+      
       <section className="container mx-auto p-6">
         <div className="flex justify-center">
           <LanguageSelect />
@@ -21,7 +28,7 @@ const PicQuizCategories = () => {
           {/* dynamically creates cards for available categories */}
           {picQuizData.map((data, index) => (
             <div
-              className="relative cursor-pointer rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105"
+              className="relative cursor-pointer rounded-lg overflow-hidden shadow-2xl transition-transform transform hover:scale-105 border-2 border-amber-300 "
               key={index + data.category}
               onClick={() => handleClick(data.category)}
             >
@@ -31,7 +38,7 @@ const PicQuizCategories = () => {
                 className="w-full h-48 sm:h-56 md:h-64 lg:h-72 object-cover"
               />
               <div className="absolute inset-0 flex items-center justify-center">
-                <p className=" text-black text-lg font-semibold px-4 py-2 rounded-lg">
+                <p className=" text-purple-700 text-4xl font-semibold px-4 py-2 rounded-lg bg-amber-300 shadow-2xl w-fit">
                   {data.category.toUpperCase()}
                 </p>
               </div>
