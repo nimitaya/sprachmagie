94b615c938c9f97940f2d76353aab5a20b0899c7
nataliia_petrukhina <natali.insajt@gmail.com>
2025-03-20 14:28:47 +0100

    LernSeite Design

--- src/pages/Lernseite.jsx
+++ src/pages/Lernseite.jsx
@@ -17,17 +17,17 @@ const Lernseite = () => {
         <LanguageSelect />
       </div>
 
-      <div className="flex flex-wrap  justify-center items-center  gap-4 mt-6 ">
+      <div className="flex flex-wrap  justify-center items-center  gap-4 lg:mt-40 sm:mt-10 ">
         {/* Karten */}
-        <div className="w-48 sm:w-56 md:w-64 lg:w-72 relative cursor-pointer rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
+        <div className="w-48 sm:w-56 md:w-64 lg:w-96 relative cursor-pointer rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
           <Link to={"/cards-categories"}>
             <img
               src="https://www.themarysue.com/wp-content/uploads/2011/04/myneighbourtotoro12.jpeg?w=448"
               alt="Karten"
-              className="w-full h-32 sm:h-40 md:h-48 object-cover"
+              className="w-full h-32 sm:h-40 md:h-48 lg:h-56 object-cover"
             />
             <div className="absolute inset-0 flex items-center justify-center  bg-opacity-50">
-              <p className="text-black text-lg font-semibold px-4 py-2 rounded-lg">
+              <p className="text-purple-700 bg-amber-300 text-xl font-semibold px-4 py-2 rounded-lg">
                 Karten
               </p>
             </div>
@@ -35,15 +35,15 @@ const Lernseite = () => {
         </div>
 
         {/* Bilder Quiz */}
-        <div className="w-48 sm:w-56 md:w-64 lg:w-72 relative cursor-pointer rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105 ">
+        <div className="w-49 sm:w-56 md:w-64 lg:w-96 relative cursor-pointer rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105 ">
           <Link to={"/picquiz"}>
             <img
               src="https://i.pinimg.com/736x/46/33/c1/4633c1b365b4421a14d7f57fc3ca9b7f.jpg"
               alt="Bilder Quiz"
-              className="w-full h-32 sm:h-40 md:h-48 object-cover"
+              className="w-full h-36 sm:h-40 md:h-48 lg:h-56  object-cover object-top"
             />
             <div className="absolute inset-0 flex items-center justify-center  bg-opacity-50 ">
-              <p className="text-black text-lg font-semibold px-4 py-2 rounded-lg">
+              <p className="text-purple-700 bg-amber-300 text-xl  font-semibold px-4 py-2 rounded-lg">
                 Bilder Quiz
               </p>
             </div>
@@ -51,15 +51,15 @@ const Lernseite = () => {
         </div>
 
         {/* Wort Quiz */}
-        <div className="w-48 sm:w-56 md:w-64 lg:w-72 relative cursor-pointer rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
+        <div className="w-48 sm:w-56 md:w-64 lg:w-96 relative cursor-pointer rounded-lg overflow-hidden shadow-lg transition-transform transform hover:scale-105">
           <Link to={"/quiz"}>
             <img
               src="https://www.tasteofcinema.com/wp-content/uploads/2014/06/Howls-Moving-Castle-2004.jpg"
               alt="Wort Quiz"
-              className="w-full h-32 sm:h-40 md:h-48 object-cover"
+              className="w-full h-32 sm:h-40 md:h-48 lg:h-56 object-cover"
             />
             <div className="absolute inset-0 flex items-center justify-center bg-opacity-50">
-              <p className="text-black text-lg font-semibold px-4 py-2 rounded-lg">
+              <p className="text-purple-700 bg-amber-300 text-xl  font-semibold px-4 py-2 rounded-lg">
                 Wort Quiz
               </p>
             </div>
