4cf0445514461af279d9dce8e8b2208ebfaa5834
Olena-prydalna <epridalnaya@gmail.com>
2025-03-20 13:28:59 +0100

    update cards&voice

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,31 @@
+import { FaVolumeUp } from "react-icons/fa";
+
 const Card = ({ words, img, language }) => {
+  // Funktion, die den Text vorliest
+  const speakWord = (word, language) => {
+    const msg = new SpeechSynthesisUtterance(word);
+    msg.lang = language; // Sprache einstellen
+    window.speechSynthesis.speak(msg); // Text vorlesen
+  };
 
   return (
-    <div className="p-4 shadow-lg rounded-lg w-full sm:w-80 md:w-96 lg:w-1/3">
+    
+    <div className="p-10 w-full sm:w-80 md:w-96 lg:w-1/3 flex flex-col items-center  bg-white/40 rounded-lg shadow-2xl relative z-10  mx-6">
       <img src={img} alt={words.de} style={{ width: "50px", height: "50px" }} />
-      <p className="text-lg font-bold">{words.de}</p>
-      <p className="text-lg font-bold">{words[language]}</p>
+      <p className="text-3xl font-bold mt-5">{words.de}</p>
+      <p className="text-3xl font-bold mt-3">{words[language]}</p>
+
+      {/* Button, der den Text vorliest */}
+      <div className="flex gap-2 mt-4">
+        <button
+          onClick={() => speakWord(words[language], language)}
+          className="bg-purple-700 p-2 text-white rounded"
+        >
+         <FaVolumeUp className="h-6 w-6" />
+        </button>
+      </div>
     </div>
+   
   );
 };
 
