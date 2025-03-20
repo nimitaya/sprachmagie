17317a45dabf0fb8e8d91dacdc7adca81b47ac43
nataliia_petrukhina <natali.insajt@gmail.com>
2025-03-20 16:38:03 +0100

    Design

--- src/pages/CardsCategoriesPage.jsx
+++ src/pages/CardsCategoriesPage.jsx
@@ -2,29 +2,75 @@ import { Link } from "react-router-dom";
 import LanguageSelect from "../components/LanguageSelect";
 
 const categories = [
-  { id: "fruits", name: "Obst" },
-  { id: "vegetables", name: "Gemüse" },
-  { id: "animals", name: "Tiere" },
-  { id: "clothes", name: "Kleidung" },
-  { id: "body", name: "Körperteile" },
-  { id: "colors", name: "Farben" },
-  { id: "weekdays", name: "Wochentage" },
-  { id: "months", name: "Monate" },
-  { id: "seasons", name: "Jahreszeiten" },
-  { id: "numbers", name: "Zahlen" },
-  { id: "transport", name: "Transportmittel" },
-  { id: "furniture", name: "Möbel" },
+  { id: "fruits", name: "Obst", img: "./public/images/categories/obst.png" },
+  {
+    id: "vegetables",
+    name: "Gemüse",
+    img: "./public/images/categories/gemüse.png",
+  },
+  { id: "animals", name: "Tiere", img: "./public/images/categories/tiere.png" },
+  {
+    id: "clothes",
+    name: "Kleidung",
+    img: "./public/images/categories/kleidung.png",
+  },
+  {
+    id: "body",
+    name: "Körperteile",
+    img: "./public/images/categories/körper.png",
+  },
+  {
+    id: "colors",
+    name: "Farben",
+    img: "./public/images/categories/farben.png",
+  },
+  {
+    id: "weekdays",
+    name: "Wochentage",
+    img: "./public/images/categories/wochentage.png",
+  },
+  {
+    id: "months",
+    name: "Monate",
+    img: "./public/images/categories/monate.png",
+  },
+  {
+    id: "seasons",
+    name: "Jahreszeiten",
+    img: "./public/images/categories/jahreszeiten.png",
+  },
+  {
+    id: "numbers",
+    name: "Zahlen",
+    img: "./public/images/categories/zahlen.png",
+  },
+  {
+    id: "transport",
+    name: "Transportmittel",
+    img: "./public/images/categories/verkehr.png",
+  },
+  {
+    id: "furniture",
+    name: "Möbel",
+    img: "./public/images/categories/möbel.png",
+  },
 ];
 
 const CardsCategoriesPage = () => {
   return (
+    // <div className="container mx-auto p-6">
     <div
-      className="flex flex-col items-center justify-center p-7"
+      className="flex-1 flex flex-col justify-center items-center bg-cover bg-center"
       style={{
-        backgroundImage: "url('https://pbs.twimg.com/media/FOtUhw6XEAYt1sP.jpg:large')",
+        backgroundImage:
+          "url('https://pbs.twimg.com/media/FOtUhw6XEAYt1sP.jpg:large')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
+        height: "100vh",
+
+        fontFamily: '"Indie Flower", cursive',
+        fontSize: "24px",
       }}
     >
       <div className="flex justify-center">
@@ -38,12 +84,12 @@ const CardsCategoriesPage = () => {
           >
             <Link to={`/cards/${category.id}`}>
               <img
-                src={`./images/categories/${category.id}.png`}
-                alt={category.name}
-                className="w-96 h-64 sm:h-72 md:h-80 lg:h-96 object-cover"
+                src={category.img}
+                alt="category.name"
+                className="w-full h-48 sm:h-56 md:h-64 lg:h-72 object-cover"
               />
-              <div className="absolute inset-0 flex items-center justify-center bg-amber-500 bg-opacity-30">
-                <p className="text-white text-xl md:text-2xl font-semibold px-4 py-2 rounded-lg">
+              <div className="absolute inset-0 flex items-center justify-center">
+                <p className=" text-black text-lg font-semibold px-4 py-2 rounded-lg">
                   {category.name}
                 </p>
               </div>
