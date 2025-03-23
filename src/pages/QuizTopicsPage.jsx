da2bb9bb4a53a60c4bacba633f79a71801217200
nataliia_petrukhina <natali.insajt@gmail.com>
2025-03-23 20:10:02 +0100

    Design

--- src/pages/QuizTopicsPage.jsx
+++ src/pages/QuizTopicsPage.jsx
@@ -25,7 +25,7 @@ const QuizTopicsPage = () => {
   return (
     <div className="page-container pb-6 flex-1">
       <LanguageSelect />
-      <h1 className="text-purple-700 text-6xl sm:text-8xl my-6 py-2 px-4 bg-purple-100/30  rounded-lg shadow-2xl w-fit">Kategorien</h1>
+      <h1 className="text-purple-700 text-4xl sm:text-8xl my-6 py-2 px-4 bg-purple-100/30  rounded-lg shadow-2xl w-fit">Kategorien</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-5xl mx-auto p-4">
         {topics.map((topic) => (
           <div
